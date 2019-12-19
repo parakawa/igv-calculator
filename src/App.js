73fc68b969c90b98e5a9dafccbd729f073e57a9d
Patricia Arakawa <paty.arakawa@gmail.com>
2019-12-19 11:56:38 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import './App.css';
+
+class App extends Component {
+	state = {  }
+	render() { 
+		return ( 
+		<div>
+			<h1>hola</h1>
+		</div>
+		);
+	}
+	}
+
+export default App;
