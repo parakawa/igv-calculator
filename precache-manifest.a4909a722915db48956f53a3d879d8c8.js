b61afce8e666c42b470b1457a86c9ae0ca8f166e
Patricia Arakawa <paty.arakawa@gmail.com>
2019-12-19 17:18:48 -0500

    Updates

---
+++ precache-manifest.a4909a722915db48956f53a3d879d8c8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9f2927faf49d05358cd79d6dc35ba8f",
+    "url": "/igv-calculator/index.html"
+  },
+  {
+    "revision": "362d4475834661c857aa",
+    "url": "/igv-calculator/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "753760fdb4f382e6790e",
+    "url": "/igv-calculator/static/css/main.ca909c41.chunk.css"
+  },
+  {
+    "revision": "362d4475834661c857aa",
+    "url": "/igv-calculator/static/js/2.cda6cd68.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/igv-calculator/static/js/2.cda6cd68.chunk.js.LICENSE"
+  },
+  {
+    "revision": "753760fdb4f382e6790e",
+    "url": "/igv-calculator/static/js/main.85c5ca4a.chunk.js"
+  },
+  {
+    "revision": "636a8b4548492fb15908",
+    "url": "/igv-calculator/static/js/runtime-main.c7cd76e0.js"
+  }
+]);
